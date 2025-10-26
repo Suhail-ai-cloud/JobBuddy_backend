26158d187a778551f0560064affe6818f922048c
Suhail-ai-cloud <nallavanayaunni12@gmail.com>
2025-10-26 10:50:27 +0530

    Add CSRF trusted origins for Netlify and Render

--- jobbuddy_project/jobbuddy_project/settings.py
+++ jobbuddy_project/jobbuddy_project/settings.py
@@ -17,8 +17,8 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost").split(",")
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://jobbuddyy.netlify.app",
-    "https://jobbuddybackend.onrender.com"
+    "https://jobbuddy-backend.onrender.com",
+    "https://jobbuddyy.netlify.app"
 ]
 
 
